5598df7938b98eebb43f5346fd55d9f81206764e
barnabas markus <barnabasmarkus@gmail.com>
2025-05-03 18:56:04 +0200

    relative image path

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 model_ckpt = "patrickjohncyh/fashion-clip"
 batch_size = 1000
 image_processing_limit = 100_000
-image_folder = "/Users/barney/Desktop/fashion-vibe/fashion-dataset/images"
+image_folder = "/fashion-dataset/images"
 index_path = "fashion_index.faiss"
 image_paths = "image_paths.pt"
